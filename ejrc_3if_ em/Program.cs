50bbe1dcb4c93e3813499b86a6229a5cde6439db
Denis <74616294@idexperujapon.edu.pe>
2025-06-20 17:02:48 -0500

    Agregar archivos de proyecto.

---
+++ ejrc_3if_ em/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejrc_3if__em
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int nota;
+            Console.WriteLine("ingresew nota");
+            nota = Convert.ToInt32(Console.ReadLine());
+            if (nota >= 16)
+            {
+                Console.WriteLine("exelente");
+            }
+            else if (nota==14|| nota == 15)
+            {
+                Console.WriteLine("muy bueno");
+            }
+            else if (nota <= 13 && nota >=11)
+            {
+                Console.WriteLine("buno");
+            }
+            else
+            {
+                Console.WriteLine("insuficiente");
+            }
+        }
+    }
+}
